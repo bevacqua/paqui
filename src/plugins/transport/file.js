c240e3d8c6d337d1932b802d4344fbed9315112a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-21 01:29:58 -0300

    serial file writer

--- src/plugins/transport/file.js
+++ src/plugins/transport/file.js
@@ -9,12 +9,13 @@ var async = require('async');
 module.exports = function (paqui) {
     return {
         transport: function (pkg, model, done) {
+            var directory = path.dirname(pkg.bin);
             var jsPath = pkg.bin + '.js';
             var jsMinPath = pkg.bin + '.min.js';
             var min = uglify.minify(model.code, { fromString: true }).code;
 
-            async.parallel([
-                async.apply(mkdirp, path.dirname(pkg.bin)),
+            async.series([
+                async.apply(mkdirp, directory),
                 async.apply(fs.writeFile, jsPath, model.code),
                 async.apply(fs.writeFile, jsMinPath, min),
             ], done);
