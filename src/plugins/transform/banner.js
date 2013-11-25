bbab280c235d2e5709bb412b81b7a59f65986c94
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2013-11-24 23:44:43 -0300

    author info banner

--- src/plugins/transform/banner.js
+++ src/plugins/transform/banner.js
@@ -6,15 +6,24 @@ var moment = require('moment');
 module.exports = function (paqui) {
     return {
         transform: function (pkg, model, done) {
+
             var banner = util.format([
                 '/*!',
                 ' * %s %s',
                 ' * %s',
                 ' *',
+                ' * Created by %s',
                 ' * Released under the %s license',
                 ' * Last modified %s',
                 ' */\n'
-            ].join('\n'), pkg.name, pkg.version, pkg.remoteUrl, pkg.license, moment().format('YYYY/MM/DD HH:mm:ss TZZ'));
+            ].join('\n'),
+                pkg.name,
+                pkg.version,
+                pkg.remoteUrl,
+                pkg.author,
+                pkg.license,
+                moment().format('YYYY/MM/DD HH:mm:ss TZZ')
+            );
 
             done(null, banner + model.code);
         }
